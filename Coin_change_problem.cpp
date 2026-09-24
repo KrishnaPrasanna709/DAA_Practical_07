05d6ed62e1e07f99f668258d1b6579d42475dc62
KrishnaPrasanna709 <krishnaprasanna.konjarla130996@marwadiuniversity.ac.in>
2026-09-24 14:51:33 +0530

    Create Coin_change_problem.cpp

---
+++ Coin_change_problem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++) {
+        for (int coin : coins) {
+            if (coin <= i) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    if (dp[amount] > amount)
+        return -1;
+
+    return dp[amount];
+}
+
+int main() {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+
+    int result = coinChange(coins, amount);
+
+    cout << "Minimum number of coins = " << result << endl;
+
+    return 0;
+}
